dde841d9acd04ba0ad8fdd1241574bf9b4e7d0db
Ádám Jakab <adam.jakab@lombiq.com>
2022-01-18 10:34:14 +0100

    Basic implementation of the steering of the vehicle

--- green_team/green_team.ino
+++ green_team/green_team.ino
@@ -7,10 +7,20 @@ enum engine_states {
   BACKWARDS
 };
 
-engine_states main_engine_state = STOP;
-int main_speed = MIN_ENGINE_SPEED;
+enum servo_states {
+  CENTER;
+  LEFT,
+  RIGHT,
+}
 
-void engine_control(uint8_t speed_pwm, engine_states state) {
+engine_states main_engine_state = STOP;
+uint16_t main_speed = MIN_ENGINE_SPEED;
+
+servo_states start_servo_state = CENTER;
+uint16_t start_servo_degree = START_SERVO_DEGREE;
+uint16_t curret_servo_degree = start_servo_degree;
+
+void engine_control(uint16_t speed_pwm, engine_states state) {
   switch (state)
   {
     case FORWARD:
@@ -33,6 +43,28 @@ void engine_control(uint8_t speed_pwm, engine_states state) {
   analogWrite(ENGINE_EN, main_speed);
 }
 
+void servo_control(uint16_t current_degrees, servo_states state, Servo myservo) {  
+  switch(state) {
+    case LEFT:
+      if(current_degrees < 135 - 5) {
+        current_degrees = current_degrees + 5;
+        myservo.write(current_degrees);
+      }
+      break;
+      
+    case RIGHT:
+      if(current_degrees > 45 + 5) {
+        current_degrees = current_degrees - 5;
+        myservo.write(current_degrees);
+      }
+      break;
+
+    default:
+      myservo.write(START_SERVO_DEGREE);
+      break;
+  }
+}
+
 void increase_speed() {
 
 }
@@ -48,8 +80,7 @@ void setup() {
   myservo.attach(SERVO_INPUT);
 
   engine_control(main_speed, main_engine_state);
-
-  myservo.write(START_SERVO_DEGREE); 
+  servo_control(START_SERVO_DEGREE, start_servo_state, myservo);
 }
 
 void loop() {
